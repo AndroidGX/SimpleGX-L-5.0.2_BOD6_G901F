90a48fef756a500ce66436062d9ee422935839cd
AndroidGX <subut3x@gmail.com>
2015-05-01 14:03:40 +0200

    Fix little mistake about build script

--- build_boot.sh
+++ build_boot.sh
@@ -9,6 +9,6 @@ find . | cpio -o -H newc | gzip > ../ramdisk.img
 cd ..
 
 tools/mkbootimg --kernel arch/arm/boot/zImage --ramdisk ramdisk.img --output boot.img \
-----cmdline "console=null androidboot.hardware=qcom user_debug=23 msm_rtb.filter=0x3b7 dwc3_msm.cpu_to_affin=1" \
+--cmdline "console=null androidboot.hardware=qcom user_debug=23 msm_rtb.filter=0x3b7 dwc3_msm.cpu_to_affin=1" \
 --base 0x00000000 --pagesize 4096 --ramdisk_offset 0x02600000 --tags_offset 0x02400000 --dt arch/arm/boot/dt.img
 
